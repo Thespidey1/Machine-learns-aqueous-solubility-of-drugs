e910ae382e71e3ce0e26d5a8ca36f294a38c68cf
Qilin Zhu <119514675+Thespidey1@users.noreply.github.com>
2024-12-22 19:30:59 +0800

    Add files via upload

---
+++ Classification models/RF/opt.py
@@ -0,0 +1,59 @@
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+from sklearn.model_selection import GridSearchCV
+
+X_train_and_valid = pd.DataFrame(
+    pd.read_excel('../X_train_and_valid_0.01.xlsx')).values
+y_train_and_valid = pd.DataFrame(
+    pd.read_excel('../y_train_and_valid.xlsx')).values
+y_train_and_valid = y_train_and_valid.ravel()
+
+scaler = StandardScaler()
+scaler = scaler.fit(X_train_and_valid)
+X_train_and_valid = scaler.transform(X_train_and_valid)
+
+
+def rf_parameters():
+    params = {'max_depth': range(10, 100, 5),
+              'n_estimators': range(100, 550, 50)}
+
+    fine_params = {
+        'min_samples_leaf': 1, 'min_samples_split': 2
+    }
+    return params, fine_params
+
+
+def model_adjust_parameters(cv_params, other_params):
+    model = RandomForestClassifier(**other_params)
+
+    optimized_param = GridSearchCV(estimator=model, param_grid=cv_params, scoring='accuracy', cv=5, verbose=3,
+                                   n_jobs=-1)
+
+    optimized_param.fit(X_train_and_valid, y_train_and_valid)
+
+    means = optimized_param.cv_results_['mean_test_score']
+    params = optimized_param.cv_results_['params']
+    std = optimized_param.cv_results_['std_test_score']
+    for mean, param, std in zip(means, params, std):
+        print("mean_score: %f,  params: %r, std: %r" % (mean, param, std))
+
+    print('参数的最佳取值：{0}'.format(optimized_param.best_params_))
+
+    print('最佳模型得分:{0}'.format(optimized_param.best_score_))
+
+    parameters_score = pd.DataFrame(data=[params, means, std])
+
+    parameters_score.to_excel('./opt_4.xlsx', index=False)
+
+    print('Optimization finished')
+
+
+if __name__ == '__main__':
+    adj_params, fixed_params = rf_parameters()
+
+    model_adjust_parameters(adj_params, fixed_params)
