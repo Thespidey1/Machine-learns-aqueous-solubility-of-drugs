a661194fa824a0b6c8a4f23f1b6cc7eebd3addcb
Qilin Zhu <119514675+Thespidey1@users.noreply.github.com>
2024-12-23 16:09:58 +0800

    opt.py

--- Regression models/XGB/opt.py
+++ Regression models/XGB/opt.py
@@ -18,7 +18,6 @@ X_train_and_valid = scaler.transform(X_train_and_valid)
 
 
 def xgb_parameters():
-    """模型调参过程"""
 
     params = {'max_depth': range(5, 21, 1),
               'learning_rate': [0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.01, 0.02, 0.03]}
@@ -28,7 +27,6 @@ def xgb_parameters():
 
 
 def model_adjust_parameters(cv_params, other_params):
-    """模型调参"""
 
     model = xgb.XGBRegressor(**other_params)
 
@@ -42,25 +40,16 @@ def model_adjust_parameters(cv_params, other_params):
     params = optimized_param.cv_results_['params']
     std = optimized_param.cv_results_['std_test_score']
     for mean, param, std in zip(means, params, std):
-        print("mean_score: %f,  params: %r, std: %r" % (mean, param, std))
+         print("mean_score: %f,  params: %r, std: %r" % (mean, param, std))
 
-    print('参数的最佳取值：{0}'.format(optimized_param.best_params_))
+    print('best parameters：{0}'.format(optimized_param.best_params_))
 
-    print('最佳模型得分:{0}'.format(optimized_param.best_score_))
-
-    parameters_score = pd.DataFrame(data=[params, means, std])
-
-    parameters_score.to_excel('./opt_2.xlsx', index=False)
+    print('best score:{0}'.format(optimized_param.best_score_))
 
     print('Optimization finished')
 
 
 if __name__ == '__main__':
-    """
-        模型调参
-        调参策略：网格搜索、随机搜索、启发式搜索
-        补充：此处采用启发式搜索，逐个或逐类参数调整，避免所有参数一起调整导致模型训练复杂度过高
-    """
 
     adj_params, fixed_params = xgb_parameters()
 
